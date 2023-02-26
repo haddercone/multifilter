c1dc664887c3a602c4f3099adbff1875221ff2db
haddercone <44robin.rs@gmail.com>
2023-02-26 21:12:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [users, setUsers] = useState([]);
+	const [isLoaded, setIsLoaded] = useState(false);
+	const [query, setQuery] = useState("");
+
+	useEffect(() => {
+		fetch("https://jsonplaceholder.typicode.com/users")
+			.then((res) => res.json())
+			.then((json) => {
+				setUsers(json);
+				setIsLoaded(true);
+			});
+	}, []);
+
+	// using derived state from users to filter the data
+	const filteredUsers = users.filter((user) => {
+		const filterUserByname = user.name
+			.toLowerCase()
+			.includes(query.toLowerCase());
+		const filterUserByEmail = user.email
+			.toLowerCase()
+			.includes(query.toLowerCase());
+		const filterUserByUserName = user.username
+			.toLowerCase()
+			.includes(query.toLowerCase());
+
+		if (filterUserByEmail) {
+			console.log("email run");
+			return filterUserByEmail;
+		} else if (filterUserByname) {
+			console.log("name run");
+			return filterUserByname;
+		} else if (filterUserByUserName) {
+			console.log("username run");
+			return filterUserByUserName;
+		} else {
+			return null;
+		}
+	});
+
+	return (
+		<div className="App">
+			<input
+				style={{ padding: "5px ", margin: "1rem" }}
+				type="text"
+				placeholder="Search Users... "
+				value={query}
+				autoFocus={true}
+				onChange={(e) => {
+					setQuery(e.target.value);
+				}}
+			/>
+			<br />
+			{!isLoaded ? (
+				"loading..."
+			) : (
+				<div>
+					<div
+						style={{
+							display: "flex",
+							justifyContent: "center",
+							fontWeight: "bold",
+						}}
+					>
+						<p style={{ width: "30%" }}>Name</p>
+						<p style={{ width: "30%" }}>User Name</p>
+						<p style={{ width: "30%" }}>Email</p>
+					</div>
+					{filteredUsers.map((user, index) => {
+						return (
+							<div
+								key={user.id}
+								style={{
+									display: "flex",
+									gap: "1rem",
+									justifyContent: "center",
+									backgroundColor: index % 2 === 0 ? "#eee  " : "white",
+								}}
+							>
+								<p style={{ width: "30%" }}>{user.name}</p>
+								<p style={{ width: "30%" }}>{user.username}</p>
+								<p style={{ width: "30%" }}>{user.email}</p>
+							</div>
+						);
+					})}
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default App;
